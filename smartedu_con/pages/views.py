11f519b1643b1759ac55bb7b00d893482ead3133
Hols <samettcaner@gmail.com>
2024-01-31 19:46:40 +0300

    django03

--- smartedu_con/pages/views.py
+++ smartedu_con/pages/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def index(req):
-    return HttpResponse("<h1> INDEX PAGE </h1>")
-def contact(req):
-    return HttpResponse("<h1> Contact PAGE </h1>")
+    return render(req, 'index.html')
